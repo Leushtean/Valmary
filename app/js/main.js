4ef2e128366ff0e7e2c0c61d9395b6f6424bed42
Leushtean <tsepordei@gmail.com>
2022-02-23 03:16:36 +0000

    added swiper block

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,22 @@
-$(function () {})
+const swiper = new Swiper('.card-swiper', {
+	observer: true,
+	observeParents: true,
+	speed: 800,
+	slidesPerView: 3,
+	spaceBetween: 55,
+	loop: true,
+	
+
+	navigation: {
+		nextEl: '.card-swiper-next',
+		prevEl: '.card-swiper-prev',
+	},
+
+	pagination: {
+		el: '.card-swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	  },
+
+	
+});
